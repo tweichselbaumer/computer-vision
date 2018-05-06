ef483e7fc65ad7a86d0d1a6af99bacd9d24135ca
Thomas Weichselbaumer <thomas.weichselbaumer@hotmail.com>
2018-05-06 21:41:12 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,102 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include <opencv2\opencv.hpp>
+#include <opencv\highgui.h>
+#include <opencv2\opencv_modules.hpp>
 
-#include "systemA\sysA.h"
-#include "systemB\sysB.h"
-#include <mathF.h>
+using namespace cv;
 
-int main(int argc, char *argv[])
+int main()
 {
-	if (argc < 2)
+	int numBoards = 0;
+	int numCornersHor;
+	int numCornersVer;
+
+	printf("Enter number of corners along width: ");
+	scanf("%d", &numCornersHor);
+
+	printf("Enter number of corners along height: ");
+	scanf("%d", &numCornersVer);
+
+	printf("Enter number of boards: ");
+	scanf("%d", &numBoards);
+
+	int numSquares = numCornersHor * numCornersVer;
+	Size board_sz = Size(numCornersHor, numCornersVer);
+
+	VideoCapture capture = VideoCapture(0);
+
+	std::vector<std::vector<Point3f>> object_points;
+	std::vector<std::vector<Point2f>> image_points;
+
+	std::vector<Point2f> corners;
+	int successes = 0;
+
+	Mat image;
+	Mat gray_image;
+	capture >> image;
+
+
+	std::vector<Point3f> obj;
+	for (int j = 0; j<numSquares; j++)
+		obj.push_back(Point3f(j / numCornersHor, j%numCornersHor, 0.0f));
+
+	while (successes<numBoards)
 	{
-		fprintf(stdout, "Usage: %s number\n", argv[0]);
-		return 1;
+		cvtColor(image, gray_image, CV_BGR2GRAY);
+
+		bool found = findChessboardCorners(image, board_sz, corners, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS);
+
+		if (found)
+		{
+			cornerSubPix(gray_image, corners, Size(11, 11), Size(-1, -1), TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1));
+			drawChessboardCorners(gray_image, board_sz, corners, found);
+		}
+
+		imshow("win1", image);
+		imshow("win2",gray_image);
+
+		capture >> image;
+		int key = waitKey(1);
+
+		if (key == 27)
+
+			return 0;
+
+		if (key == ' ' && found != 0)
+		{
+			image_points.push_back(corners);
+			object_points.push_back(obj);
+
+			printf("Snap stored!");
+
+			successes++;
+
+			if (successes >= numBoards)
+				break;
+		}
 	}
 
-	sysAadd(1, 1);
-	sysBadd(1, 1);
+	Mat intrinsic = Mat(3, 3, CV_32FC1);
+	Mat distCoeffs;
+	std::vector<Mat> rvecs;
+	std::vector<Mat> tvecs;
 
-	double inputValue = atof(argv[1]);
-	double outputValue = sqrt(inputValue);
-	fprintf(stdout, "The square root of %g is %g\n",
-		inputValue, outputValue);
+	intrinsic.ptr<float>(0)[0] = 1;
+	intrinsic.ptr<float>(1)[1] = 1;
+
+	calibrateCamera(object_points, image_points, image.size(), intrinsic, distCoeffs, rvecs, tvecs);
+
+	Mat imageUndistorted;
+	while (1)
+	{
+		capture >> image;
+		undistort(image, imageUndistorted, intrinsic, distCoeffs);
+
+		imshow("win1", image);
+		imshow("win2", imageUndistorted);
+		waitKey(1);
+	}
+
+	capture.release();
+
 	return 0;
 }
